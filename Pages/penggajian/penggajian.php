36b17493f0acdfc1edff7cbf7f5acddc0eb6d24d
pujilestari23 <164911985+pujilestari23@users.noreply.github.com>
2024-06-13 15:59:46 +0700

    pertemuan 3

---
+++ Pages/penggajian/penggajian.php
@@ -0,0 +1,9 @@
+<div id="top" class="row mb-3">
+    <div class="col">
+        <h3>Penggajian</h3>
+    </div>
+    <div class="col">
+        <a href="#" class="btn btn-success float-end">
+            <i class="fa fa-plus
+    </div>
+</div>
